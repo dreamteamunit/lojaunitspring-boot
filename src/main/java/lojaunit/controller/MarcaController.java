5246cc6299b3d831c78eb68cf6d4469e6351b124
Andre <andre.fillipe@mc2performa.com.br>
2021-01-07 12:06:27 -0200

    tentando novamente enviar o post com json

--- src/main/java/lojaunit/controller/MarcaController.java
+++ src/main/java/lojaunit/controller/MarcaController.java
@@ -9,6 +9,7 @@ import javax.validation.Path.Node;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
@@ -31,10 +34,10 @@ public class MarcaController {
 	
 	private MarcaRepository marcaRepository;
 	
-	@PostMapping(path="/add")
+	@RequestMapping(value="/add", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> addNewMarca(@Valid
-			@RequestParam String nome,
-			@RequestParam String descricao) {
+			@RequestBody String nome,
+			@RequestBody String descricao) {
 		try {
 			Marca marca = new Marca();
 			marca.setNome(nome);
