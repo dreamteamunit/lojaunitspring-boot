96cb3cfa709d8a547011b6d0e8b45c3889bfa0fe
Julio Cesar <rasjuliocesar@gmail.com>
2021-01-09 18:15:54 -0300

    Fornecedor Controller para Angular

--- src/main/java/lojaunit/controller/FornecedorController.java
+++ src/main/java/lojaunit/controller/FornecedorController.java
@@ -10,19 +10,24 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import lojaunit.entities.Fornecedor;
+import lojaunit.entities.Marca;
 import lojaunit.repository.FornecedorRepository;
 
 @Controller
@@ -32,20 +37,21 @@ public class FornecedorController {
 	
 	private FornecedorRepository fornecedorRepository;
 	
-	@PostMapping(path="/add")
-	public ResponseEntity<String> addNewFornecedor(@Valid
-			@RequestParam String nome,
+	@RequestMapping(value="/add", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public <T> ResponseEntity<T> addNewFornecedor(@Valid
+			@RequestBody Fornecedor fornecedor
+			/*@RequestParam String nome,
 			@RequestParam String endereco,
 			@RequestParam String telefone,
 			@RequestParam String cnpj,
-			@RequestParam String email) {
+			@RequestParam String email*/) {
 		
-		Fornecedor fornecedor = new Fornecedor();
+		/*Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setNome(nome);
 		fornecedor.setEndereco(endereco);
 		fornecedor.setTelefone(telefone);
 		fornecedor.setCnpj(cnpj);
-		fornecedor.setEmail(email);
+		fornecedor.setEmail(email);*/
 		try {
 			fornecedorRepository.save(fornecedor);
 		}catch(ConstraintViolationException e) {
@@ -55,15 +61,16 @@ public class FornecedorController {
 			for (Node node : violation.getPropertyPath()) {
 			    field += node.getName();
 			}
+			return (ResponseEntity<T>) new ResponseEntity<String>("Falha no cadastro de fornecedor.Campo faltando:"+field,HttpStatus.BAD_REQUEST);
 			/*throw new ResponseStatusException(
 			           HttpStatus.BAD_REQUEST, "Falha no cadastro de fornecedor.Campo faltando:"+field);*/
-			return new ResponseEntity<String>("Falha no cadastro de fornecedor.Campo faltando:"+field,HttpStatus.BAD_REQUEST);
-		}catch(DataIntegrityViolationException e) {
-			/*throw new ResponseStatusException(
-			           HttpStatus.BAD_REQUEST, "Falha no cadastro do fornecedor.Cnpj já cadastrado");*/
-			return new ResponseEntity<String>("Falha no cadastro do fornecedor.Cnpj já cadastrado",HttpStatus.BAD_REQUEST);
+			//return new ResponseEntity<String>("Falha no cadastro de fornecedor.Campo faltando:"+field,HttpStatus.BAD_REQUEST);
+		/*}catch(DataIntegrityViolationException e) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Falha no cadastro do fornecedor.Cnpj já cadastrado");
+			return new ResponseEntity<String>("Falha no cadastro do fornecedor.Cnpj já cadastrado",HttpStatus.BAD_REQUEST);*/
 		}
-		return new ResponseEntity<String>("Fornecedor Salvo com Sucesso!",HttpStatus.CREATED);
+		return (ResponseEntity<T>) new ResponseEntity<Fornecedor>(fornecedor,HttpStatus.CREATED);
 	}
 	
 	@GetMapping(path="/all")
@@ -91,7 +98,7 @@ public class FornecedorController {
 		return "Fornecedor não encontrado";
 	}
 	
-	@PutMapping(path="/update/{id}")
+	/*@PutMapping(path="/update/{id}")
 	public @ResponseBody String updateFornecedorById(
 			@RequestParam String nome,
 			@RequestParam String endereco,
@@ -111,5 +118,14 @@ public class FornecedorController {
 			return "Fornecedor atualizado com Sucesso!";
 		}
 		return "Fornecedor não encontrado";
+	}*/
+	
+	@RequestMapping(value="/update/{id}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public <T> ResponseEntity<T> updateFornecedorById(@RequestBody Fornecedor fornecedor, @PathVariable("id") Integer id){
+		if(fornecedorRepository.existsById(id)) {
+			fornecedorRepository.save(fornecedor);
+			return (ResponseEntity<T>) new ResponseEntity<Fornecedor>(fornecedor,HttpStatus.OK);
+		}
+		return (ResponseEntity<T>) new ResponseEntity<String>("Falha na atualização do fornecedor",HttpStatus.BAD_REQUEST);
 	}
 }
