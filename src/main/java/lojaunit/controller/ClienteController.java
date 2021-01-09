d535f9d9e65cf5c784f0a75d8f5a23c8561b185f
Julio Cesar <rasjuliocesar@gmail.com>
2021-01-09 16:55:54 -0300

    Cliente Repository para Angular

--- src/main/java/lojaunit/controller/ClienteController.java
+++ src/main/java/lojaunit/controller/ClienteController.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,12 +35,15 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	@PostMapping(path = "/add")
-	public ResponseEntity<String> addNewCliente(@Valid @RequestParam String nome, @RequestParam String cpf,
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/add", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public <T> ResponseEntity<T> addNewCliente(@Valid
+			@RequestBody Cliente cliente
+			/*@RequestParam String nome, @RequestParam String cpf,
 			@RequestParam String email, @RequestParam Date dataNascimento, @RequestParam String sexo,
-			@RequestParam String nomeSocial, @RequestParam String apelido, @RequestParam String telefone) {
+			@RequestParam String nomeSocial, @RequestParam String apelido, @RequestParam String telefone*/) {
 
-		Cliente cliente = new Cliente();
+		/*Cliente cliente = new Cliente();
 		cliente.setNome(nome);
 		cliente.setCpf(cpf);
 		cliente.setEmail(email);
@@ -45,7 +51,7 @@ public class ClienteController {
 		cliente.setSexo(sexo);
 		cliente.setNomeSocial(nomeSocial);
 		cliente.setApelido(apelido);
-		cliente.setTelefone(telefone);
+		cliente.setTelefone(telefone);*/
 		try {
 			clienteRepository.save(cliente);
 		}catch(ConstraintViolationException e) {
@@ -54,17 +60,19 @@ public class ClienteController {
 			String field = "";
 			for (Node node : violation.getPropertyPath()) {
 			    field += node.getName();
-			}
+			} 
+			return (ResponseEntity<T>) new ResponseEntity<String>("Falha no cadastro do cliente. Campo faltando:"+field,HttpStatus.BAD_REQUEST);
 			/*throw new ResponseStatusException(
 			           HttpStatus.BAD_REQUEST, "Falha no cadastro do cliente.Campo faltando:"+field);*/
-			return new ResponseEntity<String>("Falha no cadastro do cliente.Campo faltando:"+field,HttpStatus.BAD_REQUEST);
+			//return new ResponseEntity<String>("Falha no cadastro do cliente.Campo faltando:"+field,HttpStatus.BAD_REQUEST);
 		}
-		catch(DataIntegrityViolationException e) {
-			/*throw new ResponseStatusException(
-			           HttpStatus.BAD_REQUEST, "Falha no cadastro do cliente.Cpf já cadastrado");*/
+		/*catch(DataIntegrityViolationException e) {
+			throw new ResponseStatusException(
+			           HttpStatus.BAD_REQUEST, "Falha no cadastro do cliente.Cpf já cadastrado");
 			return new ResponseEntity<String>("Falha no cadastro do cliente.Cpf já cadastrado",HttpStatus.BAD_GATEWAY);
-		}
-		return new ResponseEntity<String>("Cliente Cadastrado com Sucesso!",HttpStatus.CREATED);
+		}*/
+		//return new ResponseEntity<String>("Cliente Cadastrado com Sucesso!",HttpStatus.CREATED);
+		return (ResponseEntity<T>) new ResponseEntity<Cliente>(cliente,HttpStatus.CREATED);
 	}
 
 	@GetMapping(path = "/all")
@@ -92,7 +100,7 @@ public class ClienteController {
 		return "O conteúdo da Tabela Clientes foi apagado com Sucesso!";
 	}
 
-	@PutMapping(path = "/update/{id}")
+	/*@PutMapping(path = "/update/{id}")
 	public @ResponseBody String updateClienteById(@RequestParam String nome, @RequestParam String cpf,
 			@RequestParam String email, @RequestParam Date dataNascimento, @RequestParam String sexo,
 			@RequestParam String nomeSocial, @RequestParam String apelido, @RequestParam String telefone,
@@ -114,5 +122,14 @@ public class ClienteController {
 		}
 
 		return "Cliente não encontrado";
+	}*/
+	
+	@RequestMapping(value="/update/{id}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public <T> ResponseEntity<T> updateClienteById(@RequestBody Cliente cliente, @PathVariable("id") Integer id){
+		if(clienteRepository.existsById(id)) {
+			clienteRepository.save(cliente);
+			return (ResponseEntity<T>) new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
+		}
+		return (ResponseEntity<T>) new ResponseEntity<String>("Falha na atualização do cliente",HttpStatus.BAD_REQUEST);
 	}
 }
